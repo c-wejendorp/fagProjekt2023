5c4dd8b2f887b21cf12742f2e6e2ee724522ce58
c-wejendorp <97293804+c-wejendorp@users.noreply.github.com>
2023-04-11 17:02:50 +0200

    Minor changes to notes_on_data

--- preprocessing/notes_on_data.py
+++ preprocessing/notes_on_data.py
@@ -1,8 +1,13 @@
 from pathlib import Path
 import mne
 
-fs_dir = Path("/mrhome/jesperdn/INN_JESPER/projects/facerecognition/freesurfer")
-data_dir = Path("/mrhome/jesperdn/INN_JESPER/projects/facerecognition_dtu/data")
+#path to the freesurfer directory
+#fs_dir = Path("/mrhome/jesperdn/INN_JESPER/projects/facerecognition/freesurfer")
+fs_dir = Path("data/freesurfer")
+
+#path to the data directory (JespersProcessed)
+#data_dir = Path("/mrhome/jesperdn/INN_JESPER/projects/facerecognition_dtu/data")
+data_dir = Path("data/JesperProcessed")
 subject = "sub-01"
 
 subject_dir = data_dir / subject
@@ -26,9 +31,9 @@ evo = mne.read_evokeds(pre_dir / "task-facerecognition_proc-p_cond-famous_split-
 evo = evo[0]
 # evo.plot(), evo.get_data()
 
-
+# orginal notes/code from Jesper below
+"""
 # Read source space data
-
 # to get from subject space to fsaverage
 # without -[l/r]h.stc !
 stc = mne.read_source_estimate("task-facerecognition_cond-famous_fwd-mne_ch-eeg_split-0_stc")
@@ -37,6 +42,19 @@ morph.apply(stc)
 # I already did this; should be equal to
 stc = mne.read_source_estimate("task-facerecognition_space-fsaverage_cond-famous_fwd-mne_ch-eeg_split-0_stc")
 # get data: stc.data, stc.lh_data, stc.rh_data
+"""
+
+#My notes/code below
+# Read source space data
+# to get from subject space to fsaverage
+# without -[l/r]h.stc !
+stc = mne.read_source_estimate(inv_dir / "task-facerecognition_cond-famous_fwd-mne_ch-eeg_split-0_stc")
+morph = mne.read_source_morph(fwd_dir / "task-facerecognition_fwd-mne_morph.h5")
+morph.apply(stc)
+# I already did this; should be equal to
+stc = mne.read_source_estimate(inv_dir / "task-facerecognition_space-fsaverage_cond-famous_fwd-mne_ch-eeg_split-0_stc")
+# get data: stc.data, stc.lh_data, stc.rh_data
+
 
 # fMRI is read in a similar manner!
 # I set time step to 2000 ms which is also repetition time (TR) so timings
