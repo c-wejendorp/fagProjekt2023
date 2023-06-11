7f78896eab8675ae865e1f1fb5c6dea338f1413c
Christoffer <wejendorp@gmail.com>
2023-06-11 15:23:28 +0200

    ..

--- MMAA/nmi.py
+++ MMAA/nmi.py
@@ -5,7 +5,10 @@ def nmi(S1, S2):
     def i(S1, S2):
         def pdd(S1, S2, k1, k2):
             #p(d,d') = ∑_n p(d|n)*p(d'|n)*p(n) (#p(d|n) = s[d,n], p(n) = 1/n)
-            # moving the 1/n to the end of the sum            
+            # moving the 1/n to the end of the sum      
+            # 
+            # 
+            return sum([S1[k1][v] * S2[k2][v] * 1 / S1.shape[1] for v in range(S1.shape[1])])      
         
             return np.array(sum([S1[k1][v] * S2[k2][v] for v in range(S1.shape[1])])) * 1 / S1.shape[1]            
         
