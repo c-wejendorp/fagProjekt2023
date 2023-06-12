af24c4d3c8446e4864b7edd56ece07c4b254beb7
Christoffer <wejendorp@gmail.com>
2023-06-12 20:20:05 +0200

    ..

--- MMAA/HPC/submit_all_scripts.sh
+++ MMAA/HPC/submit_all_scripts.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-bsub < submit_scripts/submit_MMAA_GPU_split-0_arg_num-0.sh
-bsub < submit_scripts/submit_MMAA_GPU_split-0_arg_num-1.sh
-bsub < submit_scripts/submit_MMAA_GPU_split-0_arg_num-2.sh
-bsub < submit_scripts/submit_MMAA_GPU_split-0_arg_num-3.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-0_arg_num-0.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-0_arg_num-1.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-0_arg_num-2.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-0_arg_num-3.sh
 
-bsub < submit_scripts/submit_MMAA_GPU_split-1_arg_num-0.sh
-bsub < submit_scripts/submit_MMAA_GPU_split-1_arg_num-1.sh
-bsub < submit_scripts/submit_MMAA_GPU_split-1_arg_num-2.sh
-bsub < submit_scripts/submit_MMAA_GPU_split-1_arg_num-3.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-1_arg_num-0.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-1_arg_num-1.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-1_arg_num-2.sh
+bsub < MMAA/HPC/submit_scripts/submit_MMAA_GPU_split-1_arg_num-3.sh
 
 
 
