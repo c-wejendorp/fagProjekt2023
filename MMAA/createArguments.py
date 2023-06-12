8f27a8a877762b5a28f3e281d34df7ae5860578e
Christoffer <wejendorp@gmail.com>
2023-06-12 21:01:19 +0200

    lets cook

--- MMAA/createArguments.py
+++ MMAA/createArguments.py
@@ -5,13 +5,13 @@ def createArguments():
         # i will implement that we read this from json file later 
         arguments = {    
         'subjects': list(range(1,17)),
-        'iterations': 5,
-        #'iterations': 300,
+        #'iterations': 5,
+        'iterations': 300,
         'archeTypeIntevalStart': 2,
         'archeTypeIntevalStop': 20,
         'archeTypeStepSize': 2,
-        'seeds': list(range(0, 11, 10)),
-        #'seeds': list(range(0, 91, 10)),
+        #'seeds': list(range(0, 11, 10)),
+        'seeds': list(range(0, 91, 10)),
         'lossRobust': True,
         'modalities': modalityComb,
                                     }
