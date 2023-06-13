123c31b26e7cfaae72f813e89848cfdc4b8ccc81
Christoffer <wejendorp@gmail.com>
2023-06-13 09:29:02 +0200

    working NMIplot, needs to be dynamic

--- MMAA/reproducibilityPlot.py
+++ MMAA/reproducibilityPlot.py
@@ -11,9 +11,15 @@ number_of_seeds = 10
 mods = ["eeg", "meg", "fmri"]
 colors = ["green", "red", "blue"]
 
-# lets start with split 0
-splits = [0,1]
-datapath = f"data/MMAA_results/multiple_runs/split_{splits[0]}/NMI/"
+path = f"data/MMAA_results/multiple_runs/{'-'.join(mods)}/"
+
+#here define which split is test and which is train
+train = 0
+test = 1
+
+# lets start with split train
+
+datapath = f"data/MMAA_results/multiple_runs/{'-'.join(mods)}/split_{train}/NMI/"
 
 # loop over modalities
 offset = 0
@@ -22,13 +28,13 @@ for idx, modality in enumerate(mods):
     #now over archetypes
     
     for k in range(2,20+1,2):
-        NMI_tuples.append(np.load(datapath + f"NMI_split-{splits[0]}_k-{k}_type-{modality}.npy"))
+        NMI_tuples.append(np.load(datapath + f"NMI_split-{train}_k-{k}_type-{modality}.npy"))
     
     # now we have a list of tuples with (mean,std) for each k
     # we want to plot the mean with error bars
     NMI_mean = [t[0] for t in NMI_tuples]
     NMI_std = [t[1] for t in NMI_tuples]
-    plt.errorbar(np.arange(2,20+1,2)+offset, NMI_mean, yerr=NMI_std, fmt='o', capsize=5,color=colors[idx], label=f"{modality}_split_{splits[0]}")
+    plt.errorbar(np.arange(2,20+1,2)+offset, NMI_mean, yerr=NMI_std, fmt='o', capsize=5,color=colors[idx], label=f"{modality}_split_{train}")
     offset += 0.3
 
 # now for the best NMI across splits for EEG and MEG
@@ -39,28 +45,22 @@ offset = 0
 for idx, modality in enumerate(["eeg", "meg"]):
     NMI_best = []
     #now over archetypes
-    for k in range(2,20+1,2):
-        NMI_best.append(max([np.load(path + f"split_{split}/NMI/NMI_split-{split}_k-{k}_type-{modality}.npy")[2] for split in splits]))
+    for k in range(2,20+1,2):        
+        NMI_best.append(max([np.load(f"data/MMAA_results/multiple_runs/{'-'.join(mods)}/split_{split}/NMI/NMI_split-{split}_k-{k}_type-{modality}.npy")[2] for split in [train, test]]))
+        #NMI_best.append(max([np.load(path + f"split_{split}/NMI/NMI_split-{split}_k-{k}_type-{modality}.npy")[2] for split in [train, test]]))
 
 
     # plot it as a dotted line
     plt.plot(np.arange(2,20+1,2)+offset, NMI_best, '--', color=colors[idx], label=f"{modality}_best btw splits")
     
-    offset += 0.3
-
-    
-
-
-    
-
-
+    offset += 0.3   
 
 plt.xticks(np.arange(2,20+1,2))    
 
 plt.legend()
 plt.xlabel("Number of archetypes")
 plt.ylabel("NMI")
-plt.savefig("MMAA/plots/NMI.png", dpi=300)
+plt.savefig(f"MMAA/plots/NMI_split={train}.png", dpi=300)
 plt.show()
 
 
