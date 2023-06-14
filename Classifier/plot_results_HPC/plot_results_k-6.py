6931c4df31d10a59a69f5e733b4d1e621aa1c9c2
dalitrifork <dali@trifork.com>
2023-06-14 14:02:37 +0200

    HPC classifier runs

--- Classifier/plot_results_HPC/plot_results_k-6.py
+++ Classifier/plot_results_HPC/plot_results_k-6.py
@@ -1,21 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from KNN_classifier import train_KNN, Nearest_Neighbor
-from Multinomial_log_reg import train_LR
 import re
 from collections import defaultdict
 
 from pathlib import Path
 import numpy as np
 from pca import pca
-from dtaidistance import dtw
 from tqdm import tqdm
 from sklearn.linear_model import LogisticRegression
 
 
 
-def train_all(archetypes=2, seed=0, reg_params=None, random_state=10):
+def train_all(archetypes=2, seed=0,modalityComb=["eeg", "meg", "fmri"], reg_params=None, random_state = 0, datapath=None):
     
     trainPath = Path("data/trainingDataSubset")
     testPath = Path("data/testDataSubset")
@@ -39,7 +36,7 @@ def train_all(archetypes=2, seed=0, reg_params=None, random_state=10):
         LR_pca_general_err_split = []
         
         # Leave one out subject cross validation
-        for test_subject_idx, test_subject in tqdm(enumerate(subjects)):
+        for test_subject_idx, test_subject in enumerate(subjects):
             all_subjects = range(1,17)
             train_subjects_idx = list(range(0,16))
             train_subjects_idx.remove(test_subject_idx)
@@ -47,7 +44,8 @@ def train_all(archetypes=2, seed=0, reg_params=None, random_state=10):
             train_subjects = subjects[:]
             train_subjects.remove(test_subject)
             
-            C = np.load(f"data/MMAA_results/multiple_runs/eeg-meg-fmri/split_{split}/C/C_split-{split}_k-{archetypes}_seed-{seed}.npy")
+            # C = np.load(f"data/MMAA_results/multiple_runs/eeg-meg-fmri/split_{split}/C/C_split-{split}_k-{archetypes}_seed-{seed}.npy")
+            C = np.load(datapath + f"{'-'.join(modalityComb)}/split_{split}/C/C_split-{split}_k-{archetypes}_seed-{seed}.npy")
             
             # pca
             if split == 0:
@@ -150,39 +148,41 @@ def train_all(archetypes=2, seed=0, reg_params=None, random_state=10):
     return reg_result_means, np.mean(LR_pca_general_err_all)
 
 
-def createLossPlot1(datapath = "data/MMAA_results/multiple_runs/eeg-meg-fmri/split_0/C/", savepath = "Classifier/plots/", inp_archetype=2, reg_params = None):
+def createLossPlot1(datapath = "data/MMAA_results/multiple_runs/", savepath = "Classifier/plots/",modalityComb=["eeg", "meg", "fmri"], reg_params=None, inp_archetype=2):
     
+    #datapath = Path(datapath) / Path(f"/{'-'.join(modalityComb)}/split_0/C/")   
+    C_datapath = datapath + f"{'-'.join(modalityComb)}/split_0/C/"   
+
     # make save diractory
     if not os.path.exists(savepath):
         os.makedirs(savepath)
         
     #open all files starting with eeg
     # Look, this looks stupid, but bear with me: LR_loss[Archetype][Reg_param][seed_result]   :)))  (sorry)
-    LR_loss = defaultdict(lambda: defaultdict(lambda: []))  
+    LR_reg_ploss = defaultdict(lambda: defaultdict(lambda: []))  
     LR_pca_loss = defaultdict(lambda: [])
 
-    for file in tqdm(os.listdir(datapath)): # I'm just going to assume that split_0 and split_1 has the same seeds and archetypes, if not, fight me >:(
+    for file in tqdm(os.listdir(C_datapath)): # I'm just going to assume that split_0 and split_1 has the same seeds and archetypes, if not, fight me >:(
         split, archetype, seed = re.findall(r'\d+', file)
         if not archetype == inp_archetype:
             continue
-        reg_result_means, LR_pca_gen_acc = train_all(archetypes=archetype, seed=seed, reg_params=reg_params)
+        reg_result_means, LR_pca_gen_acc = train_all(archetypes=archetype, seed=seed, reg_params=reg_params, modalityComb=modalityComb, datapath=datapath)
         
         LR_pca_loss[archetype].append(LR_pca_gen_acc)
         
-        for reg_p, mean_res in reg_result_means: 
-            LR_loss[archetype][reg_p].append(mean_res)
+        for reg_p, mean_res in reg_result_means.items(): 
+            LR_reg_ploss[archetype][reg_p].append(mean_res)
         
-        f = open("Classifier/checkpoints_{}.txt", "a")
+        f = open(f"Classifier/checkpoints_{'-'.join(modalityComb)}_k-{archetype}.txt", "a")
         print(f"____________Checkpoint archetype: {archetype}, seed: {seed}__________", file=f)
         print("LR_pca_loss = " + str(dict(LR_pca_loss)), file=f)
-        print("LR_loss = " + str({k: dict(v) for k, v in dict(LR_loss).items()}), file=f)
+        print("LR_reg_results = " + str({k: dict(v) for k, v in dict(LR_reg_ploss).items()}), file=f)
         f.close()
-    # # # best_reg_param = max(reg_result_means, reg_result_means.get)
-    # # # best_reg_result = reg_result_means[best_reg_param]
+    
     # idk why, I just randomly call it loss instead of accuracy all the time
-    f = open("Classifier/results.txt", "a")
-    print("LR_pca_loss = " + str(LR_pca_loss), file=f)
-    print("LR_loss = " + str(LR_loss), file=f)
+    f = open(f"Classifier/results_{'-'.join(modalityComb)}_k-{archetype}.txt", "a")
+    print("LR_pca_loss = " + str(dict(LR_pca_loss)), file=f)
+    print("LR_reg_results = " + str({k: dict(v) for k, v in dict(LR_reg_ploss).items()}), file=f)
     f.close()
 
 
@@ -191,16 +191,25 @@ def createLossPlot1(datapath = "data/MMAA_results/multiple_runs/eeg-meg-fmri/spl
     # LR_pca_loss =  {'10': [0.43749999999999994, 0.38541666666666663, 0.4895833333333333, 0.40625, 0.38541666666666663, 0.41666666666666663, 0.40625, 0.44791666666666663, 0.44791666666666663, 0.375], '12': [0.3958333333333333, 0.34375, 0.375, 0.35416666666666663, 0.41666666666666663, 0.44791666666666663, 0.32291666666666663, 0.3958333333333333, 0.4583333333333333, 0.40625], '14': [0.41666666666666663, 0.40625, 0.4375, 0.375, 0.38541666666666663, 0.40625]}
     # LR_loss =  {'10': [0.4583333333333333, 0.44791666666666663, 0.47916666666666663, 0.4583333333333333, 0.44791666666666663, 0.44791666666666663, 0.43749999999999994, 0.41666666666666663, 0.44791666666666663, 0.46875], '12': [0.40625, 0.4895833333333333, 0.44791666666666663, 0.44791666666666663, 0.48958333333333326, 0.41666666666666663, 0.4583333333333333, 0.4270833333333333, 0.45833333333333326, 0.44791666666666663], '14': [0.4895833333333333, 0.44791666666666663, 0.4375, 0.44791666666666663, 0.4375, 0.4583333333333333]}
 
-
-
-
+    LR_best = defaultdict(lambda: {})
+    LR_loss = defaultdict(lambda: [])
+    for archetype, reg_dict in LR_reg_ploss.items():
+        LR_reg_results= {}
+        for reg_p, seed_results in reg_dict.items():
+            LR_reg_results[reg_p] = np.mean(seed_results)
+        
+        best_reg_param = max(LR_reg_results, LR_reg_results.get)
+        best_reg_result = LR_reg_results[best_reg_param]
+        
+        LR_best[archetype][best_reg_param] = best_reg_result
+        LR_loss[archetype].append(best_reg_result)
+        
+    f = open(f"Classifier/results_{'-'.join(modalityComb)}_k-{archetype}.txt", "a")
+    print("_________Best choice of regularization parameters and their results_________")
+    print("LR_best = " + str(dict(LR_best)))
+    f.close()
+    
     # calculate the mean and std for each number of archetypes 
-    KNN_pca_mean = np.array([[archetype, np.mean(loss)] for archetype, loss in KNN_pca_loss.items()], dtype="float64")
-    KNN_pca_std = np.array([np.std(loss) for archetype, loss in KNN_pca_loss.items()])
-    
-    KNN_mean = np.array([[archetype, np.mean(loss)] for archetype, loss in KNN_loss.items()], dtype="float64")
-    KNN_std = np.array([np.std(loss) for archetype, loss in KNN_loss.items()])
-    
     LR_pca_mean = np.array([[archetype, np.mean(loss)] for archetype, loss in LR_pca_loss.items()], dtype="float64")
     LR_pca_std = np.array([np.std(loss) for archetype, loss in LR_pca_loss.items()])
     
@@ -208,22 +217,24 @@ def createLossPlot1(datapath = "data/MMAA_results/multiple_runs/eeg-meg-fmri/spl
     LR_std = np.array([np.std(loss) for archetype, loss in LR_loss.items()])
     
     #plot the mean values with std
-    plt.errorbar(KNN_pca_mean[:,0], KNN_pca_mean[:,1], yerr = KNN_pca_std, label = "KNN_pca")
-    plt.errorbar(KNN_mean[:,0], KNN_mean[:,1], yerr = KNN_std, label = "KNN")
     plt.errorbar(LR_pca_mean[:,0], LR_pca_mean[:,1], yerr = LR_pca_std, label = "LR_pca")
     plt.errorbar(LR_mean[:,0], LR_mean[:,1], yerr = LR_std, label = "LR")
     plt.legend()
     # make the x ticks integers 
-    plt.xticks(KNN_pca_mean[:,0])
+    plt.xticks(LR_pca_mean[:,0])
     plt.title("Final classification loss for different number of archetypes training data")
     plt.xlabel("Number of archetypes")
     plt.ylabel("Final loss")
-    plt.savefig(savepath + "class_error.png")    
-    plt.show()    
+    plt.savefig(savepath + f"class_error__{'-'.join(modalityComb)}.txt.png")    
+    #plt.show()     
 
    
    
 if __name__ == "__main__":
-    createLossPlot1()
+    reg_params = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+    inp_archetype = "6"
+    # datapath = "data/MMAA_results/multiple_runs/"
+    data_path_HPC = "/work3/s204090/data/MMAA_results/multiple_runs/"
+    createLossPlot1(datapath=data_path_HPC, modalityComb=["eeg", "meg", "fmri"], inp_archetype=inp_archetype, reg_params=reg_params)
     #close all plots
     plt.close("all")
