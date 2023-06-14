aa4a274ddef251dbe04a242eb5e894369aa63c7f
Christoffer <wejendorp@gmail.com>
2023-06-14 14:16:55 +0200

    new submitscript

---
+++ Classifier/submit_all_scripts_classifier.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-2.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-4.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-6.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-8.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-10.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-12.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-14.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-16.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-18.sh
+bsub < Classifier/HPC/submit_scripts/submit_Classifier_k-20.sh
+
+
+
+
+
