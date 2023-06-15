610f91144335e4f898f33092465be0cb690c4dc6
Christoffer <wejendorp@gmail.com>
2023-06-15 08:12:43 +0200

    for ruunning classifier on k 22-40

--- Classifier/submit_all_scripts_classifier.sh
+++ Classifier/submit_all_scripts_classifier.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-2.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-4.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-6.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-8.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-10.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-12.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-14.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-16.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-18.sh
-bsub < Classifier/submit_scripts/submit_Classifier_k-20.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-2.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-4.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-6.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-8.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-10.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-12.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-14.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-16.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-18.sh
+# bsub < Classifier/submit_scripts/submit_Classifier_k-20.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-22.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-24.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-26.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-28.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-30.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-32.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-34.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-36.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-38.sh
+bsub < Classifier/submit_scripts/submit_Classifier_k-40.sh
 
 
 
